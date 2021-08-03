6072bbea6091fbc844a64b40133c357d5e40493d
John Macy <john@synapse.ws>
2021-08-03 14:14:26 -0500

    Initial Commit

---
+++ SignosInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignosInterview
+//
+//  Created by John Macy on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
